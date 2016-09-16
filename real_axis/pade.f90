94775514b71030c18da1d050579f7dbe2aedcb94
Jan Berges <jan.berges@uni-bremen.de>
2016-09-16 12:08:29 +0200

    Calculate continued fraction successively from 'tail to head'

--- real_axis/pade.f90
+++ real_axis/pade.f90
@@ -6,16 +6,14 @@ module real_axis_pade
    public :: coefficients, continuation
 
    integer :: n, p
-
-   complex(dp), parameter :: i = (0, 1)
-
    complex(qp), allocatable :: c(:, :)
-   complex(qp), allocatable :: d(:, :)
 
 contains
 
    subroutine coefficients(z, u)
       real(dp), intent(in) :: z(:), u(:)
+
+      complex(dp), parameter :: i = (0, 1)
 
       n = size(z)
 
@@ -30,23 +28,22 @@ contains
 
          c(p, p - 1) = -i * z(p - 1) * c(p, p)
       end do
-
-      if (allocated(d)) deallocate(d)
-      allocate(d(2, 0:n))
-
-      d(1, 0) = 0
-      d(1, 1) = c(1, 1)
-      d(2, :) = 1
    end subroutine coefficients
 
    function continuation(x)
       complex(dp) :: continuation
       real(dp), intent(in) :: x
 
-      do p = 2, n
-         d(:, p) = d(:, p - 1) + d(:, p - 2) * (c(p, p - 1) + c(p, p) * x)
+      complex(qp) :: frac
+
+      frac = 1
+
+      do p = n, 2, -1
+         frac = 1 + (c(p, p) * x + c(p, p - 1)) / frac
       end do
 
-      continuation = cmplx(d(1, n) / d(2, n), kind=dp)
+      frac = c(1, 1) / frac
+
+      continuation = cmplx(frac, kind=dp)
    end function continuation
 end module real_axis_pade
