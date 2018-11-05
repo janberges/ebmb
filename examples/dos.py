bcf1822f2f23def0e285034cb20f97236bc23c78
Jan Berges <jan.berges@uni-bremen.de>
2018-11-05 11:47:49 +0100

    Add example showing normal and superconducting density of states

---
+++ examples/dos.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import ebmb
+import matplotlib.pyplot as plt
+
+e, dos = ebmb.square_dos('dos.in')
+
+results = ebmb.get(
+   tell          = False,
+   normal        = False,
+   dos           = 'dos.in',
+   mu            = 0.5 * e[-1],
+   lower         = 1.6 * e[ 0],
+   upper         = 0.6 * e[-1],
+   infinitesimal = 1e-4,
+   resolution    = 10001,
+   )
+
+plt.plot(e, dos)
+plt.plot(results['omega'] + results['mu'], results['DOS'])
+
+plt.show()
