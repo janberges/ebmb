ca9d8c191ad765ec90a8bfe6675d9ec464497890
Jan Berges <jan.berges@uni-bremen.de>
2021-01-25 13:16:34 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as README:
+    long_description = README.read()
+
+setuptools.setup(
+    name                          = 'ebmb',
+    version                       = '2021.1',
+    author                        = 'Jan Berges',
+    author_email                  = '',
+    description                   = 'Solve multiband Eliashberg equations',
+    long_description              = long_description,
+    long_description_content_type = 'text/markdown',
+    url                           = 'https://bitbucket.org/berges/ebmb',
+    py_modules                    = ['ebmb'],
+    python_requires               = '>=2.7',
+    install_requires              = [],
+    classifiers                   = [
+        'Programming Language :: Python',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX :: Linux',
+        ],
+    )
