cc469e05051b24427918a4dd221e891a136db871
Jan Berges <jan.berges@uni-bremen.de>
2025-01-07 15:19:47 +0100

    Update years of copyright

--- ebmb.py
+++ ebmb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2016-2024 Jan Berges
+# Copyright (C) 2016-2025 Jan Berges
 # This program is free software under the terms of the GNU GPLv3 or later.
 
 """Wrapper and auxiliary functions for Eliashberg solver ebmb"""
--- src/io_store.f90
+++ src/io_store.f90
@@ -1,4 +1,4 @@
-! Copyright (C) 2016-2024 Jan Berges
+! Copyright (C) 2016-2025 Jan Berges
 ! This program is free software under the terms of the GNU GPLv3 or later.
 
 module io_store
