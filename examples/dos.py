b0f45390621fb107f566c2e8d9b1f11077657aa0
Jan Berges <jan.berges@uni-bremen.de>
2025-01-22 17:28:28 +0100

    Plot spectra in example relative to Fermi level

--- examples/dos.py
+++ examples/dos.py
@@ -5,12 +5,14 @@ import matplotlib.pyplot as plt
 
 e, dos = ebmb.square_dos('dos.in')
 
+mu = 0.5
+
 results = ebmb.get(
    tell=False,
    normal=False,
    lamda=2.0,
    dos='dos.in',
-   mu=0.5,
+   mu=mu,
    lower=-0.25,
    upper=+0.25,
    eta=1e-4,
@@ -20,7 +22,7 @@ results = ebmb.get(
 
 print('Integral of DOS: %g' % results['states'])
 
-plt.fill_between(e, dos, color='lightgray')
-plt.plot(results['omega'] + results['mu'], results['DOS'])
+plt.fill_between(e - mu, dos, color='lightgray')
+plt.plot(results['omega'], results['DOS'])
 
 plt.show()
