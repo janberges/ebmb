025ea1763b0f32b959411e5c7c8d4f2400e227a1
Jan Berges <jan.berges@uni-bremen.de>
2025-11-14 10:56:39 +0100

    Separate plots of self-energy on different axes

--- examples/selfen.py
+++ examples/selfen.py
@@ -20,17 +20,20 @@ Tc = ebmb.get('critical', **parameters)
 
 results = ebmb.get(T=0.5 * Tc, **parameters)
 
-fig, ax = plt.subplots(3, sharex='col')
+fig, ax = plt.subplots(3, 2, sharex='col', sharey='row')
 
 for x, X in zip(ax, ['Z', 'chi', 'Delta']):
-    x.plot(results['iomega'], results[X], 'bo')
-    x.plot(results['omega'], results['Re[%s]' % X], 'r-')
-    x.plot(results['omega'], results['Im[%s]' % X], 'r--')
+    x[0].plot(results['omega'], results['Re[%s]' % X], 'r-')
+    x[0].plot(results['omega'], results['Im[%s]' % X], 'r--')
+    x[1].plot(results['iomega'], results[X], 'bo')
 
-    x.set_ylabel('$Z$' if X == 'Z' else r'$\%s$ (eV)' % X)
-    x.grid()
+    x[0].set_ylabel('$Z$' if X == 'Z' else r'$\%s$ (eV)' % X)
+    x[1].set_xlim(0.0, parameters['upper'])
 
-plt.xlabel(r'$\omega$ (eV)')
-plt.xlim(0.0, parameters['upper'])
+    x[0].grid()
+    x[1].grid()
+
+ax[-1, 0].set_xlabel(r'$\omega$ (eV)')
+ax[-1, 1].set_xlabel(r'$\omega_n$ (eV)')
 
 fig.savefig('selfen.png')
